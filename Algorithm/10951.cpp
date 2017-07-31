81e131d2bd45509377c4144388d79b693d2ed1b0
Jimin Kim <jimin6407@naver.com>
2017-07-31 18:47:06 +0900

    10951 100%

---
+++ Algorithm/10951.cpp
@@ -0,0 +1,20 @@
+//
+//  10951.cpp
+//  Algorithm
+//
+//  Created by Jimin Kim on 2017. 7. 31..
+//  Copyright © 2017년 Jimin Kim. All rights reserved.
+//
+//  A+B-4
+//  https://www.acmicpc.net/problem/10951
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int a,b;
+    while(scanf("%d %d", &a, &b) == 2)
+        printf("%d\n",a+b);
+    return 0;
+}
