8b0155969214c8c6c2fdc79ebfc880d69ae09298
Jimin Kim <jimin6407@naver.com>
2017-07-31 19:26:10 +0900

    11719 100%

---
+++ Algorithm/11719.cpp
@@ -0,0 +1,23 @@
+//
+//  11719.cpp
+//  Algorithm
+//
+//  Created by Jimin Kim on 2017. 7. 31..
+//  Copyright © 2017년 Jimin Kim. All rights reserved.
+//
+//  그대로 출력하기2
+//  https://www.acmicpc.net/problem/11719
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    char a;
+    
+    while((a = getchar()) && a != EOF) {
+        printf("%c", a);
+    }
+    
+    return 0;
+}
