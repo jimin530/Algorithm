4fab904c2e1e0bc4c29460cfb340dd4cb29f08b0
Jimin Kim <jimin6407@naver.com>
2017-07-31 19:21:28 +0900

    11718 100%

---
+++ Algorithm/11718.cpp
@@ -0,0 +1,23 @@
+//
+//  11718.cpp
+//  Algorithm
+//
+//  Created by Jimin Kim on 2017. 7. 31..
+//  Copyright © 2017년 Jimin Kim. All rights reserved.
+//
+//  그대로 출력하기
+//  https://www.acmicpc.net/problem/11718
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    char a[999];
+    
+    while(scanf("%[^\n]\n", a) != EOF) {
+        printf("%s\n", a);
+    }
+    
+    return 0;
+}
