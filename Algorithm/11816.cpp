4b6194225fb91068a3b794b7b27814a23f862067
Jimin Kim <jimin6407@naver.com>
2017-07-31 18:21:08 +0900

    11816 100%

---
+++ Algorithm/11816.cpp
@@ -0,0 +1,17 @@
+//
+//  11816.cpp
+//  Algorithm
+//
+//  Created by Jimin Kim on 2017. 7. 31..
+//  Copyright © 2017년 Jimin Kim. All rights reserved.
+//
+
+#include <stdio.h>
+int main(void) {
+    int value = 0;
+    
+    scanf("%i", &value);
+    printf("%d", value);
+    
+    return 0;
+}
