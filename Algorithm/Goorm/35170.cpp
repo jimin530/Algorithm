bfc1818d93e6565a0daf419cf5a3a797dd3d247a
Jimin Kim <jimin6407@naver.com>
2017-10-06 23:07:10 +0900

    35170.cpp

--- Algorithm/Goorm/35170.cpp
+++ Algorithm/Goorm/35170.cpp
@@ -7,7 +7,7 @@
 //
 //  사천성 시뮬레이터 (1/2)
 /*  https://edu.goorm.io/learn/exam/6245/%EC%99%84%EB%A3%8C-2%EC%B0%A8-2015%EB%85%84-%EA%B8%B0%EC%B6%9C-%EB%AC%B8%EC%A0%9C-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-9%EC%9B%94-19%EC%9D%BC/quiz/35170/%EC%82%AC%EC%B2%9C%EC%84%B1-%EC%8B%9C%EB%AE%AC%EB%A0%88%EC%9D%B4%ED%84%B0-1-2
- */
+*/
 //
 
 #include <cstdio>
@@ -16,25 +16,28 @@
 #include <vector>
 #include <string.h>
 #include <queue>
+#include <tuple>
 
 using namespace std;
 
 int dx[] = {0,0,-1,1};
 int dy[] = {-1,1,0,0};
 int a[11][11];
+bool isRoute = false;
 
 void bfs(int x1, int y1, int x2, int y2) {
     bool check[11][11];
-    int cnt = 0;
-    int flag = -1; // 왼쪽 0, 오른쪽 1, 위쪽 2, 아래쪽 3
+    // int flag = -1; // 왼쪽 0, 오른쪽 1, 위쪽 2, 아래쪽 3
     
-    queue<pair<int, int>> q;
-    q.push(make_pair(x1, y1));
+    queue<tuple<int, int, int, int>> q;
+    q.push(make_tuple(x1, y1, -1, 0)); // 행, 열, 방향, 길 수
     check[x1][y1] = true;
     
     while(!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
+        int x = get<0>(q.front());
+        int y = get<1>(q.front());
+        int direction = get<2>(q.front());
+        int cnt = get<3>(q.front());
         q.pop();
         
         for(int k = 0 ; k < 4 ; k++) {
@@ -43,36 +46,25 @@ void bfs(int x1, int y1, int x2, int y2) {
             // 범위 안에 있으면
             if (0 <= nx && nx < 10 && 0 <= ny && ny < 10) {
                 // 무늬가 없고 간 적이 없으면
-                if(a[nx][ny] == 0 && check[nx][ny] == false) {
+                if( (a[nx][ny] == 0 || (nx == x2 && ny == y2) ) && check[nx][ny] == false) {
                     
-                    // 방향이 꺾이면
-                    if(flag != k) {
-                        cnt++;
-                        q.push(make_pair(nx, ny));
+                    // 방향이 꺾이고, 현재까지 줄 수가 2개 이하일 때
+                    if(direction != k && cnt <= 2 ) {
+                        q.push( make_tuple( nx, ny, k, cnt + 1 ) );
                         check[nx][ny] = true;
                         
-                        if(cnt > 3) {
-                            printf("NO\n");
-                            return;
-                        }
-                        
                         if(nx == x2 && ny == y2) {
-                            printf("YES\n");
+                            isRoute = true;
                             return;
                         }
                     }
                     // 방향이 그대로면
-                    else {
-                        q.push(make_pair(nx, ny));
+                    else if(direction == k) {
+                        q.push( make_tuple( nx, ny, k, cnt ) );
                         check[nx][ny] = true;
                         
-                        if(cnt > 3) {
-                            printf("NO\n");
-                            return;
-                        }
-                        
                         if(nx == x2 && ny == y2) {
-                            printf("YES\n");
+                            isRoute = true;
                             return;
                         }
                     }
@@ -94,40 +86,21 @@ int main() {
     
     while(n--) {
         int x1, y1, x2, y2;
-        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+        cin >> x1 >> y1 >> x2 >> y2;
         if(a[x1-1][y1-1] == a[x2-1][y2-1]) {
             bfs(x1-1, y1-1, x2-1, y2-1);
+            
+            if(isRoute) {
+                printf("YES\n");
+            } else {
+                printf("NO\n");
+            }
         }
         else {
             printf("NO\n");
         }
-        
-        //        for(int i=0; i<10; i++){
-        //            memset(check[i], 0, sizeof(int)*10); //모든 값 0으로 초기화
-        //        }
+        isRoute = false;
     }
     
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
