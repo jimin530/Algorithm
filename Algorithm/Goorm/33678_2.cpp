0f3d184dcc6c2cae9e73faf006e73b53ecffed0a
Jimin Kim <jimin6407@naver.com>
2017-10-10 12:54:50 +0900

    33678.cpp (bfs, timeout)

--- Algorithm/Goorm/33678_2.cpp
+++ Algorithm/Goorm/33678_2.cpp
@@ -5,7 +5,7 @@
 //  Created by Jimin Kim on 2017. 10. 10..
 //  Copyright © 2017년 Jimin Kim. All rights reserved.
 //
-//  경로 추정하기
-/*  https://edu.goorm.io/learn/exam/6404/%EC%83%81%EC%8B%9C-%EC%9D%91%EC%8B%9C-1%EC%B0%A8-%EB%AA%A8%EC%9D%98-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8/quiz/33663/%EA%B2%BD%EB%A1%9C-%EC%B6%94%EC%A0%95%ED%95%98%EA%B8%B0
+//  영역 교차 검사하기
+/*  https://edu.goorm.io/learn/exam/6404/%EC%83%81%EC%8B%9C-%EC%9D%91%EC%8B%9C-1%EC%B0%A8-%EB%AA%A8%EC%9D%98-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8/quiz/33678/%EC%98%81%EC%97%AD-%EA%B5%90%EC%B0%A8-%EA%B2%80%EC%82%AC%ED%95%98%EA%B8%B0
  */
 //
